0e9eace66e28a9a6242c431713b20d44eb0a6c35
matthew <matthew1260@gmail.com>
2018-08-29 14:41:05 +0200

    Create chat but it is not working

---
+++ ChatApp/app/channels/chat_channel.rb
@@ -0,0 +1,13 @@
+class ChatChannel < ApplicationCable::Channel
+  def subscribed
+     stream_from "room_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak(data)
+    ActionCable.server.broadcast 'room_channel', data['message']
+  end
+end
